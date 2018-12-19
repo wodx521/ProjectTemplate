e9cfc4a2a0a6bd6a3a6499b3d96095cf6618b568
Administrator <wodx521@163.com>
2018-12-19 15:25:41 +0800

    修改了版本号等统一在根系统中维护

--- framelibrary/src/main/java/com/wanou/framelibrary/glidetools/GlideUtils.java
+++ framelibrary/src/main/java/com/wanou/framelibrary/glidetools/GlideUtils.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.widget.ImageView;
 
+import com.bumptech.glide.GenericTransitionOptions;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.bumptech.glide.request.transition.DrawableCrossFadeFactory;
@@ -18,39 +19,6 @@ import java.io.File;
  * Date on 2018/11/8.
  */
 public class GlideUtils {
-
-    private static GlideRequest<Drawable> glideRequest;
-
-    /**
-     * 设置图片给ImageView带圆角
-     *
-     * @param object      图片地址
-     * @param placeImage  占位图 不设置传0
-     * @param errorImage  加载错误图片 不设置传0
-     * @param roundCorner 图片圆角大小 不设置传0
-     * @param imageView   显示图片view
-     */
-    public static void intoImage(Object object, int placeImage, int errorImage, int roundCorner, ImageView imageView) {
-        GlideRequest<Drawable> glideRequest = getRequest(object);
-        if (placeImage > 0 && errorImage > 0 && roundCorner > 0) {
-            glideRequest = glideRequest.placeholder(placeImage)
-                    .error(errorImage)
-                    .transform(new RoundedCorners(roundCorner));
-        } else {
-            if (placeImage > 0) {
-                glideRequest = glideRequest.placeholder(placeImage);
-            }
-            if (errorImage > 0) {
-                glideRequest = glideRequest.error(errorImage);
-            }
-            if (roundCorner > 0) {
-                glideRequest = glideRequest.transform(new RoundedCorners(roundCorner));
-            }
-        }
-        glideRequest.transition(DrawableTransitionOptions.withCrossFade(new DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true)))
-                .into(imageView);
-    }
-
     @NonNull
     private static GlideRequest<Drawable> getRequest(Object object) {
         GlideRequest<Drawable> glideRequest;
@@ -83,6 +51,38 @@ public class GlideUtils {
      * 设置图片给ImageView带圆角
      *
      * @param object      图片地址
+     * @param placeImage  占位图 不设置传0
+     * @param errorImage  加载错误图片 不设置传0
+     * @param roundCorner 图片圆角大小 不设置传0
+     * @param imageView   显示图片view
+     */
+    public static void intoImage(Object object, int placeImage, int errorImage, int roundCorner, ImageView imageView) {
+        GlideRequest<Drawable> glideRequest = getRequest(object);
+        if (placeImage > 0 && errorImage > 0 && roundCorner > 0) {
+            glideRequest = glideRequest.placeholder(placeImage)
+                    .error(errorImage)
+                    .transform(new RoundedCorners(roundCorner));
+        } else {
+            if (placeImage > 0) {
+                glideRequest = glideRequest.placeholder(placeImage);
+            }
+            if (errorImage > 0) {
+                glideRequest = glideRequest.error(errorImage);
+            }
+            if (roundCorner > 0) {
+                glideRequest = glideRequest.transform(new RoundedCorners(roundCorner));
+            }
+        }
+        glideRequest.transition(DrawableTransitionOptions.withCrossFade(new DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true)))
+                .transition(GenericTransitionOptions.withNoTransition())
+                .into(imageView);
+    }
+
+
+    /**
+     * 设置图片给ImageView带圆角
+     *
+     * @param object        图片地址
      * @param placeDrawable 占位图 不设置传0
      * @param errorDrawable 加载错误图片 不设置传0
      * @param roundCorner   图片圆角大小 不设置传0
@@ -106,6 +106,7 @@ public class GlideUtils {
             }
         }
         glideRequest.transition(DrawableTransitionOptions.withCrossFade(new DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true)))
+                .transition(GenericTransitionOptions.withNoTransition())
                 .into(imageView);
     }
 }
